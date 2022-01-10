79e29353e7ef5fe7a23372a1f1fb69b877ce55b5
RyanWolfDev <80709113+RyanWolfDev@users.noreply.github.com>
2022-01-10 20:13:45 -0300

    Update Connection.php

--- app/connection/Connection.php
+++ app/connection/Connection.php
@@ -2,9 +2,9 @@
 
 	function iniciarConexao()
 	{
-	    $dsn  = 'mysql:host=localhost;dbname=db_secretaria;charset=utf8';
-	    $user = 'root';
-	    $pass = '';
+	    $dsn  = 'mysql:host=bwcwcpl3yegpyf4kipnn-mysql.services.clever-cloud.com;dbname=bwcwcpl3yegpyf4kipnn;charset=utf8';
+	    $user = 'uoex36a4w9rjgik0';
+	    $pass = 'c8GiHQ2roKlfp01sk0gh';
 
 	    try{
 	        $conn = new PDO($dsn, $user, $pass);
